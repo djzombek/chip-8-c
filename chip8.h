f6f1f7a244e47e1c98ed40684a697e41ca0e5abc
danny <danny@pop-os.localdomain>
2023-05-17 17:40:34 -0400

    most opcodes finished, basis for emulator complete

---
+++ chip8.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct cpu {
+    uint16_t opcode;
+    uint8_t memory[4096];
+    uint8_t V[16];
+    uint16_t i;
+    uint16_t pc;
+    uint8_t gfx[64*32];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    uint16_t stack[16];
+    uint16_t sp;
+    uint8_t key[16];
+};
